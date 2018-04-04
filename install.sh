dc22fe845a8dfb94a8c542cacc62ccf004f74c11
Dennis Ligtenberg <dennis_ligtenberg@hotmail.com>
2018-04-04 12:47:05 +0200

    removed git update

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
-
 #symlinks for dotfiles
 
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
