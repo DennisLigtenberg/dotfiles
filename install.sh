db8fbcf20e61f148ebd5d0d9cc584d8b13bdb3ef
Dennis Ligtenberg <dennis_ligtenberg@hotmail.com>
2018-04-04 13:08:04 +0200

    Added bin

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
+#!/usr/bin/env bash
+export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Make utilities available
 
 PATH="$DOTFILES_DIR/bin:$PATH"
 
+# Update Repository
+
+if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
+
 #symlinks for dotfiles
 
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
