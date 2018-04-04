50de9d02ca96fd5ed094c5d3af73c01cb2f1b5c4
Dennis Ligtenberg <dennis@localhost.localdomain>
2018-04-04 12:15:37 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
+
+#symlinks for dotfiles
+
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
 ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
